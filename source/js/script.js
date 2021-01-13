7e14192aa2ee8fcef7683581c957e7ddeebf43b0
sealione1311 <sealione@gmail.com>
2021-01-13 23:10:27 +0300

    начинает разметку

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,21 @@
-// Это пример файла со скриптами. Можете писать здесь код, который будет на странице.
+new Swiper('.special-offers', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    simulateTouch: true,
+    
+    
+    
+
+});
+
+
+
+ new Swiper('.menu-item', {
+    slidesPerView: 1.355,
+    spaceBetween:31,
+    centeredSlides: true,
+    loop: true,
+
+ })
